1115a72c878a07de232b2432b54aaa0e0845fb5a
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-10 22:06:14 -0400

    reom

--- src/homeWork/GitHomework.java
+++ src/homeWork/GitHomework.java
@@ -3,18 +3,6 @@ package homeWork;
 public class GitHomework {
 
 
-	public void display () {
-		System.out.println("John Cena");
-		
-	}
 
-	/**
-
- 
-	This is coming from GitHub
- 	Java is fun
-  Git is easy 
-  API is not Fun...
-
-	**/
+	
 } 
