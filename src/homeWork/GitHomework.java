6a63bd5a3ed1219cf5b27b8590c49d0716b28fe8
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-10 21:15:21 -0400

    remove code

--- src/homeWork/GitHomework.java
+++ src/homeWork/GitHomework.java
@@ -1,36 +1,6 @@
 package homeWork;
 
 public class GitHomework {
-	public static void main(String[] args) {
-	// System.out.println(a+b);
-		int age = 12;
-		String name= "John";
-		// This is coming from Github 
-		
-	}
-	public static void add (int a, int b) {
-		System.out.println(a+b);
-	}
-	
-	public static void add2 (int a, int b) {
-		System.out.println(a+b);
 
-}
-	public static void scrumTeam (String N, String X ) {
-		System.out.println(N);
-		System.out.println(X);
-}
 	
-	public  void Ahmed (String A, String H ) {
-		System.out.println(A);
-		System.out.println(H);	
-}
-	public  void Zahraa (String Z, String H ) {
-		System.out.println(Z);
-		System.out.println(H);		
-	
-}
-	public  void Lelia (String L, String H ) {
-		System.out.println(L);
-		System.out.println(H);	
-}
+} 
