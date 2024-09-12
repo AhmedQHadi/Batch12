a6195e54f27c459523a378e30a1ce1dd6a7bb6b1
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-12 17:49:21 -0400

    removed all codes

--- src/homeWork/GitHomework.java
+++ src/homeWork/GitHomework.java
@@ -2,31 +2,5 @@ package homeWork;
 
 public class GitHomework {
 
-public void getcity () {
-	System.out.println("Reston");
 }
-public static void main(String[] args) {
-	System.out.println("Batch 12: Git class Lecture");
-}
-
-	public static void number (int a, int b) {
-		System.out.println(a+b);
-	}
-	
-	public static void Ahmed (String x, int b) {
-		System.out.println(x+b);
-}
-
-	public static void zozo (String z, int n) {
-		System.out.println(z+n);
-}
-
-	public static void lolo (String l, int q) {
-		System.out.println(l+q);
-}	
-// we added this by GitHub
-	// add#2 by GitHub to practice pull 
-	
-}
-// This note added by my coworker via GitHub
 
