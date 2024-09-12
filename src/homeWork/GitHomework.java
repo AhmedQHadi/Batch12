7f462170e44c457b27a8c074c112aaa445258e18
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-12 17:28:09 -0400

    added another methods

--- src/homeWork/GitHomework.java
+++ src/homeWork/GitHomework.java
@@ -10,10 +10,16 @@ public static void main(String[] args) {
 		System.out.println(a+b);
 	}
 	
-	
+	public static void Ahmed (String x, int b) {
+		System.out.println(x+b);
 }
 
+	public static void zozo (String z, int n) {
+		System.out.println(z+n);
+}
 
+	public static void lolo (String l, int q) {
+		System.out.println(l+q);
+}	
 
-	
-
+}
