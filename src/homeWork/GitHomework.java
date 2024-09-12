f097054227afd8d2e8ad295c072fbbd7d7bedeb6
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-12 17:23:24 -0400

    added new Method

--- src/homeWork/GitHomework.java
+++ src/homeWork/GitHomework.java
@@ -4,10 +4,15 @@ public class GitHomework {
 
 public static void main(String[] args) {
 	System.out.println("Batch 12: Git class Lecture");
+}
+
+	public static void number (int a, int b) {
+		System.out.println(a+b);
+	}
 	
 	
 }
-}
+
 
 
 	
